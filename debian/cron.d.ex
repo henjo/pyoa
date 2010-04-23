60b394ae20ccf2baf206b074f6ded21b04c2b0c9
Henrik Johansson <henrik@arcturus.(none)>
2010-04-23 08:58:30 +0200

    Deleted unused debian control files

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pyoa package
-#
-0 4	* * *	root	[ -x /usr/bin/pyoa_maintenance ] && /usr/bin/pyoa_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(pyoa):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="pyoa" command="/usr/bin/pyoa"
